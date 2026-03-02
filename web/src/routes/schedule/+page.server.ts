71ec45abd917b5270835a7b7e70d70621a595fdb
Xel Lu <xellu@duck.com>
2026-03-03 00:18:33 +0700

    fixes?

--- web/src/routes/schedule/+page.server.ts
+++ web/src/routes/schedule/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from './$types';
 import type { WeekSchedule } from '$lib/models/Timetables';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-    const r = await fetch('/api/v1/schedule/timetables');
+    const r = await fetch('http://127.0.0.1:8100/api/v1/schedule/timetables');
 
     if (!r.ok) {
         return { timetables: [] as WeekSchedule[], scheduleError: 'errors.scheduleNetworkError' };
